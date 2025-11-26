8d57428bb1b2c89947e9e7c41aa5aa001380ed6b
Bruno Cocco <brunoe.cocco@gmail.com>
2025-11-27 00:43:41 +0100

    haciendo cartera

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 import { useState } from 'react';
 
 function App() {
-  const [vista, setVista] = useState("");
+  const [vista, setVista] = useState("productos");
 
   return (
     <>
--- src/layouts/Cartera.jsx
+++ src/layouts/Cartera.jsx
@@ -1,7 +1,46 @@
-function Cartera(){
-    return(
-        <h2>hola</h2>
-    )
+import { useState } from "react";
+
+function Cartera() {
+    const [dinero , setDinero]= useState(0)
+    const [operaciones, setOper]= useState([])
+    
+    function AgregarDinero (){
+        setDinero(dinero + 10);
+        setOper(prev => [...prev, "+10"]);
+    }
+      function RetirarDinero (){
+        setDinero(dinero - 10);
+        setOper(prev => [...prev, "-10"]);
+    }
+
+  return (
+    <>
+<section className="cartera">
+  {/* Bloque principal (saldo + botones) */}
+  <article className="cartera__panel">
+    <h1>Cartera de Inversión</h1>
+
+    <div className="cartera__saldo">
+      <h3>Tu saldo es: ${dinero}</h3>
+    </div>
+
+    <div>
+      <button onClick={AgregarDinero}>Agregar $</button>
+      <button onClick={RetirarDinero}>Retirar $</button>
+    </div>
+  </article>
+
+  {/* Bloque lateral (historial de operaciones) */}
+  <article className="cartera__historial">
+    <h2>Historial</h2>
+    {operaciones.map((op, i) => (
+      <h3 key={i}>Operación: {op}</h3>
+    ))}
+  </article>
+</section>
+
+</>
+  );
 }
 
 export default Cartera;
